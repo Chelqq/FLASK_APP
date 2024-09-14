5242116d955a71b56515249063270cf333ded8eb
Chel <cdeszz8@gmail.com>
2024-09-14 17:48:26 -0600

    added validators to fields

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import Email, DataRequired, length
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import Email, DataRequired, Length
 
 # login and registration
 
@@ -10,19 +10,20 @@ from wtforms.validators import Email, DataRequired, length
 class LoginForm(FlaskForm):
     username = StringField('Username',  
                          id='username_login',
-                         validators=[DataRequired(), length(min=2, max=20)])
+                         validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password',
                              id='pwd_login',
-                             validators=[DataRequired(), length(min=2, max=20)])
+                             validators=[DataRequired(), Length(min=2, max=20)])
+    remember = BooleanField('Remember me')
 
 
 class CreateAccountForm(FlaskForm):
     username = StringField('Username',
                          id='username_create',
-                         validators=[DataRequired(), length(min=2, max=20)])
+                         validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                       id='email_create',
                       validators=[DataRequired(), Email()])
     password = PasswordField('Password',
                              id='pwd_create',
-                             validators=[DataRequired(), length(min=2, max=20)])
+                             validators=[DataRequired(), Length(min=2, max=20)])
